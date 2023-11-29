96ca94be827faba52809d0e00fe445ac669e2925
Leandro <leamgon@gmail.com>
2023-11-29 13:31:25 -0300

    First commit

---
+++ src/test/java/agencias/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package agencias.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
