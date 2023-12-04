b70e9922c0615eaae4281ccf3e367345f2e3658d
GraceDietz <gradietz1@gmail.com>
2023-12-04 19:36:17 -0300

    se agrego AerolineaNotFoundException

--- src/main/java/agencias/service/service/impl/AerolineaServiceImpl.java
+++ src/main/java/agencias/service/service/impl/AerolineaServiceImpl.java
@@ -1,5 +1,6 @@
 package agencias.service.service.impl;
 
+import agencias.service.exceptions.AerolineaNotFoundException;
 import agencias.service.models.dto.Request.AerolineaRequestDTO;
 import agencias.service.models.dto.Response.AerolineaResponseDTO;
 import agencias.service.models.entity.Aerolinea;
@@ -43,7 +44,7 @@ public class AerolineaServiceImpl  implements AerolineaService {
 
     // Verificar si la aerolínea existe en el repositorio
     Aerolinea aerolinea = repoLinea.findById(id)
-            .orElseThrow(() -> new RuntimeException("No existe la aerolínea que desea editar"));
+            .orElseThrow(() -> new AerolineaNotFoundException("No existe la aerolínea que desea editar"));
 
     // Actualizar los atributos de la aerolínea con los valores del AerolineaRequestDTO
     aerolinea.setRazonSocial(aerolineaRequest.getRazonSocial());
@@ -66,12 +67,10 @@ public class AerolineaServiceImpl  implements AerolineaService {
     @Override
     public AerolineaResponseDTO borrarAerolinea(Long idAerolinea) {
         Aerolinea aerolinea = repoLinea.findById(idAerolinea).orElseThrow(() -> {
-            throw new RuntimeException("No existe la Aerolinea que desea eliminar");
+            throw new AerolineaNotFoundException("No existe la Aerolinea que desea eliminar");
         });
-        String nombreAerolineaEliminada = aerolinea.getRazonSocial(); // Obtén el nombre de la aerolínea eliminada
-        /*hacer el delete de la aerolinea*/
-
-        repoLinea.deleteById(idAerolinea);
+        String nombreAerolineaEliminada = aerolinea.getRazonSocial(); // Obtener el nombre de la aerolínea eliminada
+        repoLinea.deleteById(idAerolinea);/*delete de la aerolinea*/
         AerolineaResponseDTO lineaDto = new AerolineaResponseDTO();
         /* aca quiero mostrar los datos de la aerolinea que se borro pero solo pude mostrar la razonSocial*/
         lineaDto.setMessage(nombreAerolineaEliminada + " Se elimino correctamente ");
@@ -82,7 +81,7 @@ public class AerolineaServiceImpl  implements AerolineaService {
     @Override
     public AerolineaResponseDTO traerAerolineaPorId(Long idAerolinea) {
         Aerolinea aerolinea = repoLinea.findById(idAerolinea)
-                .orElseThrow(() -> new RuntimeException("No existe la Aerolínea que desea buscar"));
+                .orElseThrow(() -> new AerolineaNotFoundException("No existe la Aerolínea que desea buscar"));
 
         AerolineaResponseDTO lineaDto = mapper.map(aerolinea, AerolineaResponseDTO.class);
         lineaDto.setMessage("Se encontró la Aerolínea buscada");
