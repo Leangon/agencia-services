26d6232152835b7b154503740801f6007b714325
Marl <winering@gmail.com>
2023-12-04 16:49:19 -0300

    Agregando tests PromocionController y PromocionService

--- src/main/java/agencias/service/controllers/PromocionController.java
+++ src/main/java/agencias/service/controllers/PromocionController.java
@@ -8,10 +8,12 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/promocion")
+@Validated
 public class PromocionController {
 
     PromocionService service;
