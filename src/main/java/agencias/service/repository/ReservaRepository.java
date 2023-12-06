c08cea509d3d5b2fd730d2d5d7383f351899f836
Marl <winering@gmail.com>
2023-12-06 17:45:41 -0300

    Agregando test de aerolineaController y correcciones varias

--- src/main/java/agencias/service/repository/ReservaRepository.java
+++ src/main/java/agencias/service/repository/ReservaRepository.java
@@ -2,6 +2,7 @@ package agencias.service.repository;
 
 import agencias.service.models.entity.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
     //public List<Reserva> findReservasByReservasBetween(LocalDate date1, LocalDate date2);
 
+    @Query("FROM Reserva r WHERE r.usuario.idUsuario = ?1")
+    List<Reserva> findReservaByIdUsuario(Long id);
 }
