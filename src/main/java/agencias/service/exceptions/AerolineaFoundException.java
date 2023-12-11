f0da16593db17b8935be4a9271283cb0d0e8b82d
Marl <winering@gmail.com>
2023-12-11 11:18:08 -0300

    Corrigiendo errores parte 6

--- src/main/java/agencias/service/exceptions/AerolineaFoundException.java
+++
@@ -1,9 +0,0 @@
-package agencias.service.exceptions;
-
-public class AerolineaFoundException extends RuntimeException{
-    public AerolineaFoundException(String message){
-        super(message);
-    }
-}
-
-
